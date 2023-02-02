9f26bff61083d1a08fcf4ef44ebf321246b7ff37
jennymaow <maojenny172gmail.com>
2023-02-02 19:16:51 +0100

    New updates

--- pages/Pokeapi/Pokeapi.js
+++ pages/Pokeapi/Pokeapi.js
@@ -1,39 +1,76 @@
 import "./Pokeapi.css";
-import axios from "axios";
+import { initContent } from "../../main";
 
 const template = () => `
 <section class="pokeapi">
-    <h1>Pokeapi</h1>
+    <button class="return" id="returnHome">⬅ Return</button>
+    <h1>PokeAPI</h1>
+    <div class="searchPokemon">
+    <input type="text" id="searchedPokemon" placeholder="Search"/>
+    <button class="searchBtn" id="searchBtn">Search</button>
+    </div>
     <div id="container"></div>
 </section>
 `;
 
-const getPokemons = () => {
-  const pokemonsList = [];
+let pokemonsList = [];
+let allPokemons = [];
+
+const getPokemons = async () => {
   for (let i = 1; i < 151; i++) {
-    axios
-      .get(`https://pokeapi.co/api/v2/pokemon/${i}`)
-      .then((res) => pokemonsList.push(res.data))
-    
+    const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${i}`);
+    const json = await res.json();
+    pokemonsList.push(json);
   }
-  /* for (let i=0; i<pokemonsList.length;i++){
-    //printPokemon(pokemonsList[i]);
-    console.log (pokemonsList[i]);
-  } */
-  
+
+  mapPokemons(pokemonsList);
+};
+
+const mapPokemons = (pokemons) => {
+  allPokemons = pokemons.map((pokemon) => ({
+    name: pokemon.name,
+    image: pokemon.sprites.front_default,
+    id: pokemon.id,
+  }));
+
+  printPokemon(allPokemons);
 };
 
 const printPokemon = (pokemons) => {
+  const container = document.querySelector("#container");
+  container.innerHTML="";
   for (const pokemon of pokemons) {
-    document.querySelector("#container").innerHTML += `
-        <img src=${pokemon.sprites.front_default} alt=${pokemon.name}>
+    const figure = document.createElement("figure");
+    figure.innerHTML =`
+        <img src=${pokemon.image} alt=${pokemon.name} />
         <h2>${pokemon.name}</h2>
-        <p>${pokemon.id}</p>
+        <p>#${pokemon.id}</p>
         `;
+    container.appendChild(figure);
   }
+};
+
+const filterPokemons = (pokemons) => {
+  const myInput = document.querySelector("#searchedPokemon");
+  const filteredPokemons = pokemons.filter((pokemon) =>
+    pokemon.name.toLowerCase().includes(myInput.value.toLowerCase())
+  );
+
+  printPokemon(filteredPokemons);
+};
+
+
+const addListeners = () => {
+  document
+    .querySelector("#returnHome")
+    .addEventListener("click", () => initContent("Home"));
+  document
+    .querySelector("#searchBtn")
+    .addEventListener("click", () => filterPokemons(allPokemons));
 };
 
 export const printTemplate = () => {
   document.querySelector("#app").innerHTML = template();
+  addListeners();
   getPokemons();
 };
